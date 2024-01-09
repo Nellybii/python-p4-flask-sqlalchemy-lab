63dbbd6deb5c96ed5c54035dbc90e19a638375d4
Nelly Chepkemoi <nellychepkem2015@gmail.com>
2024-01-09 12:22:34 +0300

    flask application

--- server/models.py
+++ server/models.py
@@ -11,13 +11,26 @@ class Zookeeper(db.Model):
     __tablename__ = 'zookeepers'
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable= False)
+    birthday= db.Column(db.DateTime, nullable= False)
+
+    animal_id=db.Column(db.Integer, db.ForeignKey('animals.id'))
 
 class Enclosure(db.Model):
     __tablename__ = 'enclosures'
 
     id = db.Column(db.Integer, primary_key=True)
+    environment = db.Column(db.String, nullable= False)
+    open_to_visitors = db.Column(db.String, nullable= False)
+
+    animal_id=db.Column(db.Integer, db.ForeignKey('animals.id'))
 
 class Animal(db.Model):
     __tablename__ = 'animals'
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable= False)
+    species= db.Column(db.String, nullable= False)
+
+    zookeeper= db.relationship('Zookeeper', backref='animal')
+    enclosure= db.relationship('Enclosure', backref='animal')
